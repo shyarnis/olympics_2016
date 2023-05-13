ae152f019d1b627549a7b4cc8580a08600bfbeaa
shyarnis <shyarnisgg88@gmail.com>
2023-05-13 17:47:56 +0545

    feat: add olympics logo

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ df = preprocessing.preprocess(df, region_df)
 
 st.sidebar.title("Olympics Analysis")
 
+# st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
+st.sidebar.image('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Olympic_flag.svg/2560px-Olympic_flag.svg.png')
 user_menu = st.sidebar.radio(
     "Select and Option", (
         "Medal Tally",
